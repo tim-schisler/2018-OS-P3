e90ad6e1b5f20d4a01ce70b439663fa8fe7093ad
Tim Schisler <theschis@gmail.com>
2018-10-02 16:27:51 -0500

    Completes command line argument processing in oss. Fills in a dummy child process that prints its PID and terminates. Begins the fork/exec section of oss.

--- oss.c
+++ oss.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<unistd.h>
-
+#include<string.h>
 
 
 void helpMsgFunction(char *);
@@ -9,13 +9,18 @@ void helpMsgFunction(char *);
 int main (int argc, char *argv[]) {
 
 	int c,
+		j,
 		index,
 		maxUserProc = 5,
-		mstrTermTime = 2;
+		mstrTermTime = 2,
+		waitint;
+	char *ossLogFileName = "ossLog.txt";
+	FILE *logfile;
+	
 
 	//Command line argument processing
 	opterr = 0;
-	while ((c = getopt (argc, argv, "hl:s:t:")) != -1)
+	while ((c = getopt (argc, argv, "hl:s:t:")) != -1) {
 	switch (c)
 	{
 	case 'h':
@@ -28,27 +33,50 @@ int main (int argc, char *argv[]) {
 		mstrTermTime = atoi(optarg);
 		break;
 	case 'l':
+		ossLogFileName = optarg;
 		break;
 	case '?':
 		if (optopt == 't' || optopt == 's')
 			fprintf (stderr, "Option -%c requires an integer argument.\n", optopt);
+		else if (optopt == 'l')
+			fprintf (stderr, "Option -%c requires a string argument.\n", optopt);
 		else if (isprint (optopt))
 			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
 		else
 			fprintf (stderr,
 			"Unknown option character `\\x%x'.\n",
 			optopt);
-			return 1;
+		return 1;
 	default:
 		abort ();
+	}
 	}
 	for (index = optind; index < argc; index++)
 		printf ("Non-option argument %s\n", argv[index]);
 
 	
+	//fork/exec
+	pid_t childpid[maxUserProc];
+	for(j = 0; j < maxUserProc; j++) {
+		switch ( childpid[j] = fork() )
+		{
+		case -1:
+			perror("forking");
+			return -1;
+		case 0:	//child
+			if( execl("./user", "./user", (char *)NULL) == -1) {
+				perror("exec-ing");
+				return -1;
+			}
+			break;
+		default:	//parent
+			wait(&waitint);
+			break;
+		}
+	}
 	
 	
-
+	
 	return 0;
 }
 
--- user.c
+++ user.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 
+int main (int argc, char *argv[]) {
 
-
-int main () {
-
-
+	printf("Child %ld reporting.\n", (long)getpid());
 
 	return 0;
 }
